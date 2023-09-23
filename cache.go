4b7647e0d44d07918510261c4f910f5bbd81e7f1
Tiger Wang <tigerwang@outlook.com>
2023-09-23 19:15:29 +0800

    wip

---
+++ cache.go
@@ -0,0 +1,35 @@
+package gocache
+
+import "time"
+
+type Cache[K comparable, V any] struct {
+	cache map[K]Expirable[V]
+}
+
+func (c Cache[K, V]) Get(key K) *V {
+	expirable, ok := c.cache[key]
+
+	if !ok {
+		return nil
+	}
+
+	if expirable.expirationTime < time.Now().UnixMilli() {
+		return nil
+	}
+
+	return &expirable.data
+}
+
+func (c *Cache[K, V]) Put(key K, value V, ttl int64) {
+	if ttl <= 0 {
+		return
+	}
+
+	c.cache[key] = NewExpirable(value, ttl)
+}
+
+func NewCache[K comparable, V any]() Cache[K, V] {
+	return Cache[K, V]{
+		cache: make(map[K]Expirable[V], 0),
+	}
+}
